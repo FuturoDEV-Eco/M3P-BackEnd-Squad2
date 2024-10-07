7cf92e63e4ef02bf2229bd98a6ab01150b803ee4
Charles Biveu <cbiveu@gmail.com>
2024-10-07 14:28:21 -0300

    rebuild database

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,17 @@
-const {Sequelize} = require('sequelize');
+const { Sequelize } = require('sequelize');
 const config = require('../config/database.config');
 
+// Inicializa a conexão com o Sequelize
 const connection = new Sequelize(config);
 
-module.exports = connection;
+// Função para sincronizar o banco de dados
+async function syncDatabase() {
+  try {
+    await connection.sync({ force: true }); // 'force: true' recria todas as tabelas
+    console.log('Tabelas recriadas com sucesso.');
+  } catch (error) {
+    console.error('Erro ao sincronizar banco de dados:', error);
+  }
+}
+
+module.exports = { connection, syncDatabase };
